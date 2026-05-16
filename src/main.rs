6097646313111c9612dac206e049097ad0c17c11
metalwhiskersx3 <metalmasterx3@proton.me>
2026-05-15 20:02:53 -0400

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use console::Term;
+use std::io::stdout;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let term = Term::stdout();
+    term.write_line("Hello World");
+    thread::sleep(Duration::from_millis(2000));
+    term.clear_line();
 }
